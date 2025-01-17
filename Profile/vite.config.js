f76b484dd738d7be7ac9beb5362d04ce60721a32
AniketDhangar <21aniketdhangar13@gmail.com>
2025-01-17 11:03:14 +0530

    new commit

--- Profile/vite.config.js
+++ Profile/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Output directory for the build
-  },
-  base: '/', // Set the base URL (adjust if hosting in a subdirectory)
 });
